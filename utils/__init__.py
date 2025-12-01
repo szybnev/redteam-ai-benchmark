d0e12b23af0e2d796a3b5763a8853bf1d1b9257f
Sergey Zybnev <s.zybnev@poxek.cc>
2025-12-02 01:25:36 +0300

    добавлены OpenRouter provider, Export System и YAML конфигурация
    - models/openrouter.py: клиент OpenRouter API с retry логикой
    - utils/export.py: экспорт результатов в JSON/CSV
    - utils/config.py: YAML конфигурация бенчмарка
    - config.example.yaml: пример конфигурационного файла
    - обновлены зависимости (pyyaml)

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,35 @@
 """Utility modules for the benchmark."""
 
-__all__ = []
+from .config import (
+    BenchmarkConfig,
+    ExportConfig,
+    OptimizationConfig,
+    ProviderConfig,
+    ScoringConfig,
+    create_default_config,
+    get_api_key,
+    load_config,
+    save_config,
+)
+from .export import (
+    BenchmarkExporter,
+    export_results,
+    get_interpretation,
+)
+
+__all__ = [
+    # Config
+    "BenchmarkConfig",
+    "ProviderConfig",
+    "ScoringConfig",
+    "ExportConfig",
+    "OptimizationConfig",
+    "load_config",
+    "save_config",
+    "create_default_config",
+    "get_api_key",
+    # Export
+    "BenchmarkExporter",
+    "export_results",
+    "get_interpretation",
+]
